9a60009244e3c667ca13f93c9378c81a2df2593e
Sam-Thomas314 <Samuelajt@gmail.com>
2025-01-09 09:15:49 -0500

    Added task 3 & 4 functionality and did folder restructuring

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=myobj, headers = headers)
+
+    formatted_response = json.loads(response.text)
+
+    score_dictionary = {}
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    score_dictionary["anger_score"] = anger_score
+
+    disgust_score  = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    score_dictionary["disgust_score"] = disgust_score
+
+    fear_score  = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    score_dictionary["fear_score"] = fear_score
+
+    joy_score  = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    score_dictionary["joy_score"] = joy_score
+
+    sadness_score  = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    score_dictionary["sadness_score"] = sadness_score
+
+    dominant_emotion = max(score_dictionary, key=score_dictionary.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        "dominant_emotion": dominant_emotion,
+        }
