b0b552519c371fe66284491b7b52ece449d9eee8
Sam-Thomas314 <Samuelajt@gmail.com>
2025-01-10 11:14:55 -0500

    The pylint score is now 10/10, changed a little with formatting server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''This is the server'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,7 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def self_analyzer():
+    '''Analyze the answer to determine if the reponse was valid or not, print answer to webpage'''
     emotion_to_detect = request.args.get('textToAnalyze')
     response = emotion_detector(emotion_to_detect)
 
@@ -17,13 +19,15 @@ def self_analyzer():
 
     if anger is None:
         return "Invalid text! Please try again"
-    
-    return """For the given statement, the system response is anger {}, disgust {},
-            fear {}, joy {}, and sadness {}. The dominanty emotion is {}.""".format(anger,
-            disgust, fear, joy, sadness, dominant_emotion)
+    return f"""
+    For the given statement, the system response is anger {anger}, disgust {disgust},
+    fear {fear}, joy {joy}, and sadness {sadness}.
+    The dominanty emotion is {dominant_emotion}
+    """
 
 @app.route("/")
 def render_index_page():
+    '''Render home page'''
     return render_template("index.html")
 
 if __name__ == "__main__":
